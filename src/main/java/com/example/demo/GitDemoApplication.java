18d1abeb760911df44fc515dd8670881971f6c05
RamKumar <ramkumarhbti@gmail.com>
2020-06-21 11:35:37 +0530

    First commit

---
+++ src/main/java/com/example/demo/GitDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Application started...");
+		SpringApplication.run(GitDemoApplication.class, args);
+	}
+
+}
